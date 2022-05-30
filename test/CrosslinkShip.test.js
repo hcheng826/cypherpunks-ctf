8b10178738640179cf8d655fd29efb1f6c28eadd
hcheng826 <lilioo826@gmail.com>
2022-05-30 18:30:40 +0800

    remove only

--- test/CrosslinkShip.test.js
+++ test/CrosslinkShip.test.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 const hre = require('hardhat');
 
-describe("CrossLink", function () {
+describe.only("CrossLink", function () {
   it("test", async function () {
     const provider = ethers.provider;
     const [deployer, player] = await hre.ethers.getSigners();
