78ca624e055dff5d34f8358422875ca668cc06f6
hcheng826 <lilioo826@gmail.com>
2022-05-30 23:09:15 +0800

    try new multiple clients

--- contracts/VentureCapital.sol
+++ contracts/VentureCapital.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.24;
 
+import 'hardhat/console.sol';
+
 contract DeveloperAuthorizer {
   bool public developerMode;
 
@@ -34,9 +36,9 @@ contract VentureCapital {
   address public owner;
   uint256 public startTime;
   uint256 public clientBalance;
-  mapping(address => bool) fundManagementEnabled;
-  mapping(address => uint256) clientIndex;
-  Client[] clients;
+  mapping(address => bool) public fundManagementEnabled;
+  mapping(address => uint256) public clientIndex;
+  Client[] public clients;
   DeveloperAuthorizer public developerAuthorizer;
 
   constructor() public payable {
@@ -67,15 +69,21 @@ contract VentureCapital {
   {
     uint256 index = clientIndex[_clientAddress];
     if (index != 0) {
+      console.log('72 index: ', index);
       Client storage client = clients[index - 1];
+      console.log('74 client.amount: ', client.amount);
       client.amount += msg.value;
       clientBalance += msg.value;
+      console.log('77 client.amount: ', client.amount);
     } else {
+      console.log('79 index: ', index);
+      console.log('80 client.amount0: ', client.amount);
       clientBalance += msg.value;
       client.manager = manager;
       client.age = _age;
       client.clientAddress = _clientAddress;
       client.amount = msg.value;
+      console.log('86 client.amount1: ', client.amount);
       clients.push(client);
       clientIndex[_clientAddress] = clients.length;
     }
@@ -101,10 +109,14 @@ contract VentureCapital {
   }
 
   function close() public isManager {
-    require(developerAuthorizer.developerMode() == false);
-    require(msg.sender == owner);
+    require(developerAuthorizer.developerMode() == false, 'developerMode is on');
+    require(msg.sender == owner, '!owner');
     require(clients.length == 0);
     msg.sender.transfer(address(this).balance);
+  }
+
+  function getClientsLength() public view returns (uint256){
+    return clients.length;
   }
 
   function() external payable {}
--- test/VentureCapital.test.js
+++ test/VentureCapital.test.js
@@ -1,3 +1,4 @@
+const { util } = require("chai");
 const { ethers } = require("hardhat");
 const hre = require('hardhat');
 
@@ -5,10 +6,11 @@ const hre = require('hardhat');
 
 // Approach 1: send ether to the pre-computed address that DeveloperAuthorizer is about to be deployed
 // Approach 2: kill the contract and re-deploy using CREATE2: https://hackernoon.com/using-ethereums-create2-nw2137q7
-describe("Venture Capital", function () {
+describe.only("Venture Capital", function () {
   it("hardhat local network", async function () {
-    const provider = hre.network.provider;
-    const [player, deployer, client] = await hre.ethers.getSigners();
+    const provider = ethers.provider;
+
+    const [player, deployer, client, client2] = await hre.ethers.getSigners();
 
     // const delpoyerNonce = await deployer.getTransactionCount();
     // console.log('delpoyerNonce', delpoyerNonce.toString(16));
@@ -39,15 +41,15 @@ describe("Venture Capital", function () {
     const da = await ethers.getContractAt(
         require('../artifacts/contracts/VentureCapital.sol/DeveloperAuthorizer.json').abi,
         await vc.developerAuthorizer(),
-        deployer
+        player
     );
 
     console.log('player.address:', player.address);
     console.log('owner.address:', await vc.owner());
     console.log('manager:', await vc.manager());
 
-    console.log('vc balance: ', await provider.send('eth_getBalance', [vc.address]));
-    console.log('da balance: ', await provider.send('eth_getBalance', [da.address]));
+    console.log('vc balance: ', await provider.getBalance(vc.address));
+    console.log('da balance: ', await provider.getBalance(da.address));
     console.log('vc client balance:', await vc.clientBalance());
 
     console.log('da address:', da.address);
@@ -58,38 +60,71 @@ describe("Venture Capital", function () {
 
     // const managementEnableTx = await vc.connect(player).managementEnable(player.address);
     // await managementEnableTx.wait();
-    console.log('vc client balance:', await vc.clientBalance());
-    console.log('vc balance: ', await provider.send('eth_getBalance', [vc.address]));
 
-    // const newClientTx = await vc.connect(player).newClient(client.address, 18, { value: 2 });
-    // await newClientTx.wait();
+    const newClientTx = await vc.connect(player).newClient(client.address, 18, { value: 1 });
+    await newClientTx.wait();
+    console.log('clients.length: ', await vc.getClientsLength());
+    console.log('clients[0]:', await vc.clients(0));
 
-    console.log('vc client balance:', await vc.clientBalance());
-    console.log('the client balance:', await provider.send('eth_getBalance', [client.address]));
-    console.log('vc balance: ', await provider.send('eth_getBalance', [vc.address]));
+    // const VCAFactory = await ethers.getContractFactory("VentureCapitalAttack");
+    // const vca = await VCAFactory.connect(player).deploy(vc.address);
+    // await vca.deployed();
+
+    const newClientTx2 = await vc.connect(player).newClient(client2.address, 20, { value: 2 });
+    await newClientTx2.wait();
+
+    console.log('clients.length: ', await vc.getClientsLength());
+    console.log('clients[0]:', await vc.clients(0));
+    console.log('clients[1]:', await vc.clients(1));
+
+    const newClientTx3 = await vc.connect(player).newClient(client.address, 17, { value: 3 });
+    await newClientTx3.wait();
+    console.log('clients.length: ', await vc.getClientsLength());
+    console.log('clients[0]:', await vc.clients(0));
+    console.log('clients[1]:', await vc.clients(1));
+
+    // const clientWithdrawTx = await vc.connect(player).clientWithdraw(vca.address);
+    // await clientWithdrawTx.wait();
 
     const byeClientTx = await vc.connect(player).byeClient();
     await byeClientTx.wait();
+    console.log('clients.length: ', await vc.getClientsLength());
 
-    console.log('vc client balance:', await vc.clientBalance());
-    console.log('the client balance:', await provider.send('eth_getBalance', [client.address]));
-    console.log('vc balance: ', parseInt(await provider.send('eth_getBalance', [vc.address])));
+    const newClientTx4 = await vc.connect(player).newClient(client.address, 18, { value: 4 });
+    await newClientTx4.wait();
+    console.log('clients.length: ', await vc.getClientsLength());
+    console.log('clients[0]:', await vc.clients(0));
 
-    const VCAFactory = await ethers.getContractFactory("VentureCapitalAttack");
-    const vca = await VCAFactory.connect(player).deploy(vc.address);
-    await vca.deployed();
+    // console.log('clients.length: ',await vc.getClientsLength());
+    // console.log('vca balance: ', await provider2.getBalance(vca.address));
+    // console.log('vc balance: ', await provider2.getBalance(vc.address));
+    // console.log('clientIndex[vca]:', await vc.clientIndex(vca.address));
+    // console.log('clients[0]:', await vc.clients(0));
+    // console.log('99 clients[-1]:', await vc.clients(-1));
 
-    console.log('vc address', vca.address);
+    // const clientWithdrawTx = await vc.clientWithdraw(vca.address);
+    // await clientWithdrawTx.wait();
 
-    const newClientTx = await vc.connect(player).newClient(vca.address, 18, { value: 100 });
-    await newClientTx.wait();
+    // console.log('clients[0]:', await vc.clients(0));
+    // console.log('clients.length: ', await vc.getClientsLength());
 
-    for (let i = 0; i < 1e16; i++) {
-        const clientWithdrawTx = await vc.connect(player).clientWithdraw(vca.address);
-        await clientWithdrawTx.wait();
+    // const byeClientTx = await vc.connect(player).byeClient();
+    // await byeClientTx.wait();
 
-        console.log('vc balance: ', parseInt(await provider.send('eth_getBalance', [vc.address])));
-        console.log('vca balance:', parseInt(await provider.send('eth_getBalance', [vca.address])));
-    }
-  });
+    /* turn off da
+    const turnOffTx = await da.connect(player).turnOff({ value: ethers.utils.parseEther('0.575758') });
+    await turnOffTx.wait();
+
+    const closeTx = await vc.close();
+    await closeTx.wait();
+    */
+
+    // const newClientTx2 = await vc.newClient(client2.address, 18, { value: 1 });
+    // await newClientTx2.wait();
+
+    // console.log('clients.length: ',await vc.getClientsLength());
+    // console.log('clientIndex[client2]:', await vc.clientIndex(client2.address));
+    // console.log('clientIndex[client]:', await vc.clientIndex(client.address));
+    // console.log('clients[0]:', await vc.clients(0));
+    });
 });
