0c616ecc09ddd4323fd6ae788a9da1f740eb88c7
hcheng826 <lilioo826@gmail.com>
2022-06-01 15:50:41 +0800

    use original contract

--- contracts/VentureCapital.sol
+++ contracts/VentureCapital.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.4.24;
 
-import 'hardhat/console.sol';
-
 contract DeveloperAuthorizer {
   bool public developerMode;
 
@@ -36,9 +34,9 @@ contract VentureCapital {
   address public owner;
   uint256 public startTime;
   uint256 public clientBalance;
-  mapping(address => bool) public fundManagementEnabled;
-  mapping(address => uint256) public clientIndex;
-  Client[] public clients;
+  mapping(address => bool) fundManagementEnabled;
+  mapping(address => uint256) clientIndex;
+  Client[] clients;
   DeveloperAuthorizer public developerAuthorizer;
 
   constructor() public payable {
@@ -52,8 +50,7 @@ contract VentureCapital {
     require(
       fundManagementEnabled[msg.sender] ||
         msg.sender == manager ||
-        developerAuthorizer.developerMode(),
-        "!isManager"
+        developerAuthorizer.developerMode()
     );
     _;
   }
@@ -69,21 +66,15 @@ contract VentureCapital {
   {
     uint256 index = clientIndex[_clientAddress];
     if (index != 0) {
-      console.log('72 index: ', index);
       Client storage client = clients[index - 1];
-      console.log('74 client.amount: ', client.amount);
       client.amount += msg.value;
       clientBalance += msg.value;
-      console.log('77 client.amount: ', client.amount);
     } else {
-      console.log('79 index: ', index);
-      console.log('80 client.amount0: ', client.amount);
       clientBalance += msg.value;
       client.manager = manager;
       client.age = _age;
       client.clientAddress = _clientAddress;
       client.amount = msg.value;
-      console.log('86 client.amount1: ', client.amount);
       clients.push(client);
       clientIndex[_clientAddress] = clients.length;
     }
@@ -109,14 +100,10 @@ contract VentureCapital {
   }
 
   function close() public isManager {
-    require(developerAuthorizer.developerMode() == false, 'developerMode is on');
-    require(msg.sender == owner, '!owner');
+    require(developerAuthorizer.developerMode() == false);
+    require(msg.sender == owner);
     require(clients.length == 0);
     msg.sender.transfer(address(this).balance);
-  }
-
-  function getClientsLength() public view returns (uint256){
-    return clients.length;
   }
 
   function() external payable {}
